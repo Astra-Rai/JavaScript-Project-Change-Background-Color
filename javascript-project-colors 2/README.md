47e62b8ba0f0400675c7b8fdc946595689930d20
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2023-01-10 14:47:07 -0500

    Add files via upload

---
+++ javascript-project-colors 2/README.md
@@ -0,0 +1,4 @@
+# JavaScript Project | Colors
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Astra_Rai/pen/PoBZrag](https://codepen.io/Astra_Rai/pen/PoBZrag).
+
---
+++ javascript-project-colors 2/dist/script.js
@@ -0,0 +1,9 @@
+var index = 0;
+
+function changeColors(){
+  var colors = [ '#F84545', '#4287f5', '#f56f42', 'f5d442', '#3bc460', 'b0a3a2'];
+  document.getElementsByTagName("body")[0].style.background = colors[index++];
+  
+  if(index > colors.length-1)
+    index = 0;
+}
